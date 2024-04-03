80071aebbaad9c4745668933ff644561e006243f
NoMercy404 <126580913+NoMercy404@users.noreply.github.com>
2024-04-03 12:28:04 +0200

    Update 1089. Duplicate Zeros.py

--- 1089. Duplicate Zeros.py
+++ 1089. Duplicate Zeros.py
@@ -1,13 +1,21 @@
-arr = [1,0,2,3,0,4,5,0]
-arr2 =[]
-for i in range(0,len(arr)-1):
-    if(arr[i] == 0):
-        arr2.append(arr[i])
-        arr2.append(0)
-    else:
-        arr2.append(arr[i])
-del arr2[len(arr):]
-arr.clear()
-for i in arr2:
-    arr.append(i)
-print(arr)
+class Solution(object):
+    def duplicateZeros(self, arr):
+        IsZero=1
+        for a in arr:
+            if(a==0):
+                IsZero+=1
+        if(IsZero==1):
+            return False
+        else:
+            arr2 =[]
+            for i in range(0,len(arr)-1):
+                if(arr[i] == 0):
+                    arr2.append(arr[i])
+                    arr2.append(0)
+                else:
+                    arr2.append(arr[i])
+            del arr2[len(arr):]
+            del arr[:]
+        for i in arr2:
+            arr.append(i)
+        return arr
